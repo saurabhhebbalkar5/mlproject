651610ba956db1f39ee54059ff52153ed355520f
saurabhhebbalkar5 <saurabhhebbalkar@gmail.com>
2023-04-28 16:12:30 +0530

    logging update

--- src/logger.py
+++ src/logger.py
@@ -14,6 +14,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-if __name__ == "__main__":
-    logging.info("Logging has begun")
+# if __name__ == "__main__":
+#     logging.info("Logging has begun")
 
